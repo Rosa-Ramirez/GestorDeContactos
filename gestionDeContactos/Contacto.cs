616b4bc06e66c7a91bad5edace7d1df26b416289
Rosa Ramirez <ramirezrosae8@gmail.com>
2024-07-02 12:57:02 -0600

    Proyecto Gestor de Contactos

---
+++ gestionDeContactos/Contacto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestionDeContactos;
+
+public class Contacto
+{
+    [Key]
+    public Guid Id {get;set;}
+    public string Nombre {get;set;}
+    public string Telefono {get;set;}
+    public string Email {get;set;}
+
+    public override string ToString()
+    {
+        return $"Id: {Id} \nNombre: {Nombre} \nTeléfono: {Telefono} \nEmail: {Email}";
+    }
+}
